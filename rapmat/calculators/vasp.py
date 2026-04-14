955c5053081ef1dd2cee60685110686abec21a79
Michael Levenets <milevev256@gmail.com>
2026-04-15 00:15:24 +1000

    Correctly direct VASP output

--- rapmat/calculators/vasp.py
+++ rapmat/calculators/vasp.py
@@ -14,4 +14,9 @@ def build_calculator_vasp(config: dict, directory: Path | None = None) -> Vasp:
     kwargs = dict(config)
     if directory is not None:
         kwargs["directory"] = str(directory)
+
+    if "txt" not in kwargs:
+        workdir = kwargs.get("directory", ".")
+        kwargs["txt"] = str(Path(workdir) / "vasp.out")
+
     return Vasp(**kwargs)
